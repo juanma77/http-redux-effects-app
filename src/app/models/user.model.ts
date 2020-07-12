d9d07c47da3c06b98dc2af3c1b76944df8780267
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-11 20:34:05 -0500

    user model added

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,20 @@
+import { Identifiers } from '@angular/compiler';
+
+export class UserModel {
+
+    public id: string; 
+    public first_name: string;
+    public last_name: string;
+    public avatar: string; 
+
+
+    constructor( id: string, first_name: string, last_name: string, avatar: string ) {
+
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.avatar = avatar; 
+
+    }
+
+}
