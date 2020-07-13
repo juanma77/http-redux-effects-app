0012c72f96368d446772c5816f30d991042f52ff
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-12 20:24:26 -0500

    users reducer added

---
+++ src/app/store/reducers/users.reducer.ts
@@ -0,0 +1,38 @@
+import { createReducer, on } from '@ngrx/store';
+import { LOAD_USERS_ACTION, LOAD_USERS_SUCCESS_ACTION, LOAD_USERS_ERROR_ACTION } from '../actions';
+
+export interface UsersState {
+    users: [],
+    loaded: boolean,
+    loading: boolean,
+    error: any 
+}
+
+export const usersInitialState: UsersState = {
+    users: [],
+    loaded: false,
+    loading: false,
+    error: null 
+}
+
+const _usersReducer = createReducer(usersInitialState,
+
+    on(LOAD_USERS_ACTION, state => ({ ...state, loading: true })),
+    on(LOAD_USERS_SUCCESS_ACTION, (state, { users } )  => ({ 
+        ...state, 
+        loading: false,
+        loaded: true,
+        users: [ ...users ] 
+    })),
+    on(LOAD_USERS_ERROR_ACTION, (state, { payload } )  => ({ 
+        ...state, 
+        loading: false,
+        loaded: false,
+        error: payload 
+    })),
+
+);
+
+export function usersReducer(state, action) {
+    return _usersReducer(state, action);
+}
