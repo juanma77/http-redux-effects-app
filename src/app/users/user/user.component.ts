639100cef91b92dbc6b12ea722e092b2ae09d8f8
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-13 22:51:13 -0500

    showing info at user view

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AppState } from 'src/app/store/app.reducers';
 import { Store } from '@ngrx/store';
 import { LOAD_USER_ACTION } from 'src/app/store/actions';
+import { UserModel } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user',
@@ -11,10 +12,20 @@ import { LOAD_USER_ACTION } from 'src/app/store/actions';
 })
 export class UserComponent implements OnInit {
 
+  public user: UserModel;      
+
   constructor( private router: ActivatedRoute, private store: Store<AppState> ) { }
 
   // Obtenemos el id de un usuario y disparamos la accion de cargar usuario
   ngOnInit() {
+
+    this.store.select('user').subscribe( user => {
+
+      this.user = user.user; 
+
+
+    })
+
 
     this.router.params.subscribe( ({ id }) => {
 
